56d70b928619569489794ff198c2b6062dce3af4
Pratik Goswami PM <54017670+puribapu9141@users.noreply.github.com>
2020-05-18 11:13:36 +0530

    Update pmlob.py

--- userbot/modules/pmlob.py
+++ userbot/modules/pmlob.py
@@ -7,7 +7,7 @@ from collections import deque
 from userbot.events import register
 
 @register(outgoing=True, pattern="^.pmlob")
-async def port_kalp(event):
+async def port_pmlob(event):
 	if event.fwd_from:
 		return
 	deq = deque(list("โค๏ธ๐งก๐’๐’๐’๐’๐–ค"))
