d5b2b053c131a2bc280d71f4c122950aa3a2dcf9
Pratik Goswami PM <54017670+puribapu9141@users.noreply.github.com>
2020-05-18 11:12:28 +0530

    Create pmlob.py

---
+++ userbot/modules/pmlob.py
@@ -0,0 +1,18 @@
+# (c) @UniBorg
+# Original written by @UniBorg edit by @PM_The_Angry
+
+from telethon import events
+import asyncio
+from collections import deque
+from userbot.events import register
+
+@register(outgoing=True, pattern="^.pmlob")
+async def port_kalp(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("โค๏ธ๐งก๐’๐’๐’๐’๐–ค"))
+	for _ in range(48):
+		await asyncio.sleep(1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
