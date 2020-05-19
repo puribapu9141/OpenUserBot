b1020acea9f102bec34d4f9655d77d4434e54bd1
Pratik Goswami PM <54017670+puribapu9141@users.noreply.github.com>
2020-05-19 21:16:46 +0530

    Update all_phone.py
    Added fatsboot, adb, aex, afh

--- userbot/modules/all_phone.py
+++ userbot/modules/all_phone.py
@@ -1,3 +1,7 @@
+#created by @PM_The_Angry
+#credit goes to @Vachounet for Permission
+#thanks @Three_Cube_TeKnoways for Bug Fixing and Helping
+
 import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
@@ -18,6 +22,67 @@ async def _(event):
           try:
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=995271804))
               await conv.send_message(f'/{fboot} {link}')
+              response = await response
+          except YouBlockedUserError:
+              await event.reply("```Unblock @android_helper_bot plox```")
+              return
+          else:
+             await event.delete()
+             await bot.forward_messages(event.chat_id, response.message)
+
+@register(outgoing=True, pattern="^.adb(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@android_helper_bot"
+    adb = f"adb"
+    await event.edit("```Processing```")
+    async with bot.conversation("@android_helper_bot") as conv:
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=995271804))
+              await conv.send_message(f'/{adb} {link}')
+              response = await response
+          except YouBlockedUserError:
+              await event.reply("```Unblock @android_helper_bot plox```")
+              return
+          else:
+             await event.delete()
+             await bot.forward_messages(event.chat_id, response.message)
+
+
+@register(outgoing=True, pattern="^.afh(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@android_helper_bot"
+    afh = f"afh"
+    await event.edit("```Processing```")
+    async with bot.conversation("@android_helper_bot") as conv:
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=995271804))
+              await conv.send_message(f'/{afh} {link}')
+              response = await response
+          except YouBlockedUserError:
+              await event.reply("```Unblock @android_helper_bot plox```")
+              return
+          else:
+             await event.delete()
+             await bot.forward_messages(event.chat_id, response.message)
+
+@register(outgoing=True, pattern="^.aex(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@android_helper_bot"
+    aex = f"aex"
+    await event.edit("```Processing```")
+    async with bot.conversation("@android_helper_bot") as conv:
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=995271804))
+              await conv.send_message(f'/{aex} {link}')
               response = await response
           except YouBlockedUserError:
               await event.reply("```Unblock @android_helper_bot plox```")
