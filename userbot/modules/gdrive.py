d6227255dc95a892c8aa444e1ab0d2ca92de64aa
Pratik Goswami PM <54017670+puribapu9141@users.noreply.github.com>
2020-05-24 06:16:46 +0530

    Update gdrive.py

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -36,7 +36,7 @@ from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 
 from userbot import (
-    G_DRIVE_DATA, G_DRIVE_CLIENT_ID, G_DRIVE_CLIENT_SECRET,
+    G_DRIVE_CLIENT_ID, G_DRIVE_CLIENT_SECRET,
     G_DRIVE_FOLDER_ID, BOTLOG_CHATID, TEMP_DOWNLOAD_DIRECTORY, CMD_HELP, LOGS,
 )
 from userbot.events import register
